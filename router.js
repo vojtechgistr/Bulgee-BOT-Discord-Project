66591a79ae3036e5295351752410d31a815422a8
VojtaG <56306485+VojtaG@users.noreply.github.com>
2020-08-13 22:29:51 +0200

    Delete router.js

--- router.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    // '/'
-    app.use('/', require('./routes/index'));
-
-    app.use('/dashboard', require('./routes/dashboard'));
-
-    // '/authorize'
-    app.use('/authorize', require('./routes/discord'));
-}
